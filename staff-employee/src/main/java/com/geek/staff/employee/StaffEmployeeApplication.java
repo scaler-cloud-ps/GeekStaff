bfad4ebe29b604f5bfc8908d310f87bd9a7be005
scaler-cloud-ps <pangsongdev@163.com>
2022-03-17 17:03:58 +0800

    极客员工代码提交

---
+++ staff-employee/src/main/java/com/geek/staff/employee/StaffEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.geek.staff.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffEmployeeApplication.class, args);
+    }
+
+}
---
+++ staff-gateway/src/test/java/com/geek/staff/gateway/StaffGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.staff.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
